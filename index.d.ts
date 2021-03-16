7502dfc423da83d38336006e98c983756e251ba1
CJ <cj@specialblend.net>
2021-03-16 14:17:54 -0500

    chore: housekeeping

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,16 @@
 export declare type None = [];
 export declare type Some<T> = [T];
 export declare type Maybe<T> = Some<T> | None;
+
+export declare namespace Lazy {
+    let of: <T>(x: T) => Lazy<T>;
+}
+
 export interface Lazy<T> {
     exec(): Promise<T>;
     map<U>(fn: (x: T) => U): Lazy<U>;
     flatMap<U>(fn: (x: T) => Lazy<U>): Lazy<U>;
 }
+
 export declare function Box<T>(x?: T): Maybe<T>;
 export declare function Lazy<T>(exec: () => Promise<T>): Lazy<T>;

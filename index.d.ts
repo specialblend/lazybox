b48b594ffa578ceabdd17f97fcfd62bff20c188f
CJ <cj@specialblend.net>
2021-03-16 13:37:11 -0500

    fix: types

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,10 @@
-export type None = [];
-export type Some<T> = [T];
-export type Maybe<T> = Some<T> | None;
-
+export declare type None = [];
+export declare type Some<T> = [T];
+export declare type Maybe<T> = Some<T> | None;
 export interface Lazy<T> {
     exec(): Promise<T>;
     map<U>(fn: (x: T) => U): Lazy<U>;
     flatMap<U>(fn: (x: T) => Lazy<U>): Lazy<U>;
 }
-
-export function Box<T>(x?: T): Maybe<T>;
-export function Lazy<T>(exec: () => Promise<T>): Lazy<T>;
+export declare function Box<T>(x?: T): Maybe<T>;
+export declare function Lazy<T>(exec: () => Promise<T>): Lazy<T>;
